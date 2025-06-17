d5e45da1a815245b4707018eafda2f49b711e68f
murilodamarioo <murilo.gcdamario@gmail.com>
2025-06-16 21:29:26 -0300

    :sparkles: feat: Define Env variables types

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
 import { CreateAdminAccountController } from './controllers/create-admin-account.controller';
+import { ConfigModule } from '@nestjs/config';
+import { envSchema } from './env';
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      validate: env => envSchema.parse(env),
+      isGlobal: true
+    })
+  ],
   controllers: [CreateAdminAccountController],
   providers: [PrismaService],
 })
