6b58348499cdd3ed8f6cf03e2bd132827a549088
murilodamarioo <murilo.gcdamario@gmail.com>
2025-06-18 09:59:08 -0300

    :sparkles: feat: Implements create order controller

--- src/controllers/create-order.controller.ts
+++ src/controllers/create-order.controller.ts
@@ -1,9 +1,17 @@
-import { CurrentUser } from '@/auth/current-user-decorator'
-import { UserPayload } from '@/auth/jwt.strategy'
+import { ZodValidationPipe } from '@/pipes/zod-validation-pipe'
 import { PrismaService } from '@/prisma/prisma.service'
-import { Controller, HttpCode, Post, UseGuards } from '@nestjs/common'
+import { Body, Controller, HttpCode, Post, UseGuards, UsePipes } from '@nestjs/common'
 import { AuthGuard } from '@nestjs/passport'
+import { z } from 'zod'
 
+
+const createOrderBodySchema = z.object({
+  orderName: z.string(),
+  recipientId: z.string().uuid(),
+  courierId: z.string().uuid()
+})
+
+type CreateOrderBodySchema = z.infer<typeof createOrderBodySchema>
 
 @Controller('/order')
 @UseGuards(AuthGuard('jwt'))
@@ -13,8 +21,17 @@ export class CreateOrderController {
 
   @Post()
   @HttpCode(201)
-  async handle(@CurrentUser() user: UserPayload) {
-    return 'OK'
+  @UsePipes(new ZodValidationPipe(createOrderBodySchema))
+  async handle(@Body() body: CreateOrderBodySchema) {
+    const { orderName, courierId, recipientId } = body
+
+    await this.prisma.order.create({
+      data: {
+        title: orderName,
+        courierId,
+        recipientId
+      }
+    })
   }
 
 }
