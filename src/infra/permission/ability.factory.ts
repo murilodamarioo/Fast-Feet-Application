ef0c0a411281decd3f629dbd8f659d0bd254c915
Murilo Damario <murilo.gcdamario@gmail.com>
2025-08-09 22:48:57 -0300

    :recycle: refactor: DefineAbilitiesFor method

--- src/infra/permission/ability.factory.ts
+++ src/infra/permission/ability.factory.ts
@@ -22,45 +22,45 @@ export type AppAbility = MongoAbility<[Action, Subject]>
 export class AbilityFactory {
 
   defineAbilitiesFor(user: any, requestParam: string) {
-    const { can, cannot, build } = new AbilityBuilder<AppAbility>(createMongoAbility)
+    const { can, cannot, build } = new AbilityBuilder<AppAbility>(createMongoAbility);
 
-    if (user.role === Role.ADMIN) {
-      can(Action.MANAGE, 'Admin')
-
+    const adminAbilities = () => {
+      can(Action.MANAGE, 'Admin');
       if (requestParam !== user.id) {
-        cannot(Action.UPDATE, 'Admin')
-        cannot(Action.DELETE, 'Admin')
+        cannot(Action.UPDATE, 'Admin');
+        cannot(Action.DELETE, 'Admin');
       }
+      can(Action.MANAGE, 'Courier');
+      can(Action.MANAGE, 'Recipient');
+      can(Action.MANAGE, 'Order');
+      cannot(Action.PICKUP, 'Order');
+      cannot(Action.DELIVER, 'Order');
+    };
 
-      can(Action.MANAGE, 'Courier')
-      cannot(Action.PENDING, 'Courier')
-      cannot(Action.PICKUP, 'Courier')
-      cannot(Action.RETURN, 'Courier')
-      cannot(Action.DELIVER, 'Courier')
-
-      can(Action.MANAGE, 'Recipient')
-      cannot(Action.PENDING, 'Recipient')
-      cannot(Action.PICKUP, 'Recipient')
-      cannot(Action.RETURN, 'Recipient')
-      cannot(Action.DELIVER, 'Recipient')
-
-      can(Action.MANAGE, 'Order')
-      cannot(Action.PICKUP, 'Order')
-      cannot(Action.DELIVER, 'Order')
-    } else if (user.role === Role.COURIER) {
+    const courierAbilities = () => {
       if (requestParam === user.id) {
-        can(Action.READ, 'Courier')
-        can(Action.UPDATE, 'Courier')
+        can(Action.READ, 'Courier');
+        can(Action.UPDATE, 'Courier');
       }
+      can(Action.READ, 'Order');
+      can(Action.PICKUP, 'Order');
+      can(Action.DELIVER, 'Order');
+      cannot(Action.RETURN, 'Order');
+    };
 
-      can(Action.READ, 'Order')
-      can(Action.PICKUP, 'Order')
-      can(Action.DELIVER, 'Order')
-      cannot(Action.RETURN, 'Order')
+    switch (user.role) {
+      case Role.ADMIN:
+        adminAbilities();
+        break;
+      case Role.COURIER:
+        courierAbilities();
+        break;
+      default:
+        break;
     }
 
     return build({
-      detectSubjectType: (item: any) => item.constructor as ExtractSubjectType<Subject>
-    })
+      detectSubjectType: (item: any) => item.constructor as ExtractSubjectType<Subject>,
+    });
   }
 }
