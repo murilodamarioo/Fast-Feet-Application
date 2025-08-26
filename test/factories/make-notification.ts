e31e89e264790da912cc3513dde79a3c4e798613
Murilo Damario <murilo.gcdamario@gmail.com>
2025-08-25 21:10:20 -0300

    :sparkles: feat: Implements Implements Notification factory class

--- test/factories/make-notification.ts
+++ test/factories/make-notification.ts
@@ -1,5 +1,10 @@
 import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+
 import { Notification, NotificationProps } from '@/domain/notification/enterprise/entities/notification'
+import { PrismaNotificationMapper } from '@/infra/database/prisma/mappers/prisma-notification-mapper'
+
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+
 import { faker } from '@faker-js/faker'
 
 export function makeNotification(override: Partial<NotificationProps> = {}, id?: UniqueEntityId) {
@@ -11,4 +16,18 @@ export function makeNotification(override: Partial<NotificationProps> = {}, id?:
   }, id)
 
   return notification
+}
+
+export class NotificationFactory {
+  constructor(private prisma: PrismaService) { }
+
+  async makePrismaNotification(data: Partial<NotificationProps> = {}): Promise<Notification> {
+    const notification = makeNotification(data)
+
+    await this.prisma.notification.create({
+      data: PrismaNotificationMapper.toPrisma(notification)
+    })
+
+    return notification
+  }
 }
